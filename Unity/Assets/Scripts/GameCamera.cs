5dc4ca4cdc52e01abb6b98cb4b2db113ae8398ce
Matt Hughson <contact@matthughson.com>
2013-03-09 00:58:13 -0800

    Smooth Cam | Ignore *.pidb
    Adding simple smoothing to the camera that follows the player to make things a little less jerky.
    
    Added *.pidb files to ignore list since those are user specific.

--- Unity/Assets/Scripts/GameCamera.cs
+++ Unity/Assets/Scripts/GameCamera.cs
@@ -3,10 +3,17 @@ using System.Collections;
 
 public class GameCamera : MonoBehaviour 
 {
+	private Vector3 TargetPos = Vector3.zero;
+	
+    public float Speed = 10.0f;
+	
     //============================================================================================================================================================================================//
     void Update()
     {
-	    // Follow Player //
-        transform.position = Game.Instance.Player.transform.position + new Vector3(0, 0, -100);
+		// Follow Player //
+		TargetPos = Game.Instance.Player.transform.position + new Vector3(0, 0, -100);
+		
+		// Slowly move from the current position to the target position.
+        transform.position = Vector3.Lerp(transform.position, TargetPos, Time.deltaTime * Speed);
 	}
 }

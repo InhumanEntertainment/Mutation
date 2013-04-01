633ead20b6371fa701ba75fa968851a306cc70d3
Erik Larsson <ecl3d@hotmail.com>
2013-04-01 13:08:52 -0600

    Teleporter

--- Unity/Assets/Scripts/CheckPoint.cs
+++ Unity/Assets/Scripts/CheckPoint.cs
@@ -3,9 +3,22 @@ using System.Collections;
 
 public class CheckPoint : MonoBehaviour 
 {
+    public bool Active = true;
+    public bool InTrigger = false;
+    public bool Activated = false;
     public int PixelHeight = 20;
     public string Level;
+    tk2dAnimatedSprite Sprite;
+    public ParticleSystem FXIdle;
+    public Animation Popup;
 
+    //============================================================================================================================================================================================//
+    void Awake()
+    {
+        Sprite = GetComponent<tk2dAnimatedSprite>();
+        SetActive(Active);
+    }
+    
     //============================================================================================================================================================================================//
     void OnDrawGizmos()
     {
@@ -14,23 +27,61 @@ public class CheckPoint : MonoBehaviour
     }
 
     //============================================================================================================================================================================================//
+    public void SetActive(bool on)
+    {
+        Active = on;
+        if (on)
+        {
+            Sprite.Play("Teleporter_Off");
+            FXIdle.Play();
+            
+        }
+        else
+        {
+            Sprite.Play("Teleporter_Off");
+            FXIdle.Stop();
+        }
+    }
+
+    //============================================================================================================================================================================================//
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && !Activated && InTrigger && Active)
+        {
+            print("Activate!!!");
+            Activated = true;
+
+           if (Level != "")
+           {
+               // Load Level //
+               Game.Instance.LoadLevel(Level);
+               Audio.PlaySound("Teleport");
+           }
+           else
+           {
+               // Save for Continue //
+               Game.Instance.Data.CurrentCheckPoint = name;
+           }
+        }
+    }
+
+    //============================================================================================================================================================================================//
     void OnTriggerEnter(Collider collider)
     {        
-        if (collider.tag == "Player")
+        if (Active && collider.tag == "Player" && !InTrigger)
         {
-            print("CheckPoint: " + name);
-        
-            if (Level != "" && collider.tag == "Player")
-            {
-                // Next Level //
-                Game.Instance.LoadLevel(Level);
-                Audio.PlaySound("Teleport");
-            }
-            else
-            {
-                // Save for Continue //
-				Game.Instance.Data.CurrentCheckPoint = name;
-            }
+            InTrigger = true;
+            Popup.PlayQueued("Teleporter_Popup_Open", QueueMode.PlayNow);
         }      
+    }
+
+    //============================================================================================================================================================================================//
+    void OnTriggerExit(Collider collider)
+    {
+        if (Active && collider.tag == "Player" && InTrigger)
+        {
+            InTrigger = false;
+            Popup.PlayQueued("Teleporter_Popup_Close", QueueMode.PlayNow);
+        }
     } 
 }

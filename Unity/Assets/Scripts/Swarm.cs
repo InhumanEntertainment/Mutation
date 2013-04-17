804a3dd2668219319e21506c391e39d4ec1f08f1
Erik Larsson <ecl3d@hotmail.com>
2013-04-17 09:16:26 -0600

    Swarm

---
+++ Unity/Assets/Scripts/Swarm.cs
@@ -0,0 +1,59 @@
+// Swarming Prototype //
+// Move away from every other entity //
+// Creat list of previous velocities //
+
+public class Entity
+{
+	public Vector3 Velocity;
+	public Vector3 Acceleration;
+	public Vector3 RandomAcceleration;
+	public float Radius;
+	
+	
+}
+
+public class EntityManager
+{
+	public GameObject EntityPrefab;
+	public List<Entity> Entities = new List<Entity>();
+	
+	public Entity CreateEntity()
+	{
+		Entity entity = Instatiate(EntityPrefab);
+		Entities.Add(entity);
+	}
+	
+	void Update()
+	{
+		Vector3[] PreviousVelocity = new Vector3[Entities.Count]
+		
+		for(int i=0; i < Entities.Count; i++)
+		{
+			PreviousVelocity[i] = Entities[i].rigidBody.Velocity;
+		}
+		
+		// Combine All Velocities //
+		// if entity is not self and is within Radius, Add Force //
+		for(int i=0; i < Entities.Count; i++)
+		{
+			for(int c=0; c < Entities.Count; c++)
+			{
+				if(i != c)
+				{
+					Vector3 dir = Entities[c] - Entities[i];
+					float length = dir.length;
+					
+					if(length < radius)
+					{
+						float strength = 1f - (length / radius);
+					
+						Vector3 force = dir * strength;
+						Entities[c].rigibody.Velocity += force;
+					}
+				}
+			}
+			
+			Ent = Entities[i].rigidBody.Velocity;
+		}
+	}
+}
